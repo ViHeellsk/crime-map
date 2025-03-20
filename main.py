a1acbc225adac933779565637da0765dcba5524a
rita4100 <mishenkorita41@gmail.com>
2025-03-20 14:24:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ import io  # Pomáhá zpracovat soubory v paměti
 
 # Inicializace FastAPI aplikace
 app = FastAPI()
+
+# Konfigurace databáze (změňte podle svého nastavení)
+db_config = {
+    "host": "mariadb",  # Název databázového serveru
+    "user": "root",  # Uživatelské jméno
+    "password": "1236",  # Heslo
+    "database": "crime_map"  # Název databáze
+}
+
+def insert_crime_data(df):
+    conn = mysql.connector.connect(**db_config)  # Připojení k databázi
+    cursor = conn.cursor()
